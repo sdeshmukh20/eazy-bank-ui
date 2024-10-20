f6badb5b440abc1091bbf3db6466baf218e5b29e
Satyajeet Deshmukh <sdeshmukh@mobiquityinc.com>
2024-10-20 11:51:30 +0530

    release-9-u11: fix sumbit contact details issue

--- src/app/components/contact/contact.component.ts
+++ src/app/components/contact/contact.component.ts
@@ -1,7 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Contact } from "src/app/model/contact.model";
 import { NgForm } from '@angular/forms';
-import { getCookie } from 'typescript-cookie';
 import { DashboardService } from 'src/app/services/dashboard/dashboard.service';
 
 
@@ -12,19 +11,23 @@ import { DashboardService } from 'src/app/services/dashboard/dashboard.service';
 })
 export class ContactComponent implements OnInit {
   model = new Contact();
+  contacts = new Array();
 
   constructor(private dashboardService: DashboardService) {
 
   }
 
   ngOnInit() {
-      
+
   }
 
   saveMessage(contactForm: NgForm) {
     this.dashboardService.saveMessage(this.model).subscribe(
       responseData => {
-        this.model = <any> responseData.body;
+        this.contacts = <any> responseData.body;
+        this.contacts.forEach(function (this: ContactComponent, contact: Contact) {
+          this.model = contact;
+        }.bind(this));
         contactForm.resetForm();
       });
 
